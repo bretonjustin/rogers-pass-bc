9ed9deee000a16a58abb63bb3fae519f97b51058
Justin Breton <jbreton@boreas.ca>
2023-12-24 10:28:55 -0500

    begin working on project

--- app/main.py
+++ app/main.py
@@ -16,3 +16,12 @@ async def home(request: Request):
         "avalanche_web_link": "https://www.avalanche.ca/forecasts/a2a7748a-d376-45c9-b6aa-272df4c22d5d_b2a14eb464567902bcdf5b0cf1efe870416fee4de1d80d5b1f6d9cbc1b3bd73a",
     }
     return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+# Get rogers pass data
+@app.get("/rogers-pass", response_class=HTMLResponse)
+async def rogers_pass(request: Request):
+    data = {
+        
+    }
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
