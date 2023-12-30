d5ed70b733c7d96c7cb79a23d7c13bdbd04531de
Justin Breton <jbreton@boreas.ca>
2023-12-30 08:54:27 -0500

    added middleware security

--- app/main.py
+++ app/main.py
@@ -5,10 +5,32 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from app import rogers_pass_bc
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "https://rogers-pass-bc.herokuapp.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(GZipMiddleware, minimum_size=500)
+
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["rogers-pass-bc.herokuapp.com", "localhost"]
+)
 
 app.include_router(rogers_pass_bc.router)
 
