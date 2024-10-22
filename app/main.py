f4eb4d7bcd096872554a093bb1efc02c6ef402ce
Justin Breton <breton.justin97@gmail.com>
2024-10-22 10:23:23 -0400

    allow github pages

--- app/main.py
+++ app/main.py
@@ -26,19 +26,19 @@ origins = [
     "http://127.0.0.1:8080",
 ]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_middleware(GZipMiddleware, minimum_size=500)
 
-# app.add_middleware(
-#     TrustedHostMiddleware, allowed_hosts=["rogers-pass-bc.herokuapp.com", "localhost"]
-# )
+app.add_middleware(
+    TrustedHostMiddleware, allowed_hosts=["rogers-pass-bc.herokuapp.com", "localhost", "https://bretonjustin.github.io"]
+)
 
 app.include_router(rogers_pass_bc.router)
 
