a948b1c4f1d470bd800769307adbf9d7f4697265
Justin Breton <breton.justin97@gmail.com>
2025-11-11 09:06:20 -0500

    Fix new snow not in all measurements.

--- app/library/weather_station.py
+++ app/library/weather_station.py
@@ -255,7 +255,7 @@ def get_weather_station_data(url: str):
                 windDirCompass=measurement["windDirCompass"],
                 windSpeedGust=measurement["windSpeedGust"],
                 relativeHumidity=measurement["relativeHumidity"],
-                newSnow=measurement["newSnow"]
+                newSnow=measurement.get("newSnow", 0.0)
             ))
 
         return measurements
